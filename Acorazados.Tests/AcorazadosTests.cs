3011360e94a4382e4fb931526eb2676aa575cf46
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-11-18 10:27:12 -0500

    🟢Si_InicializoUnTablero_Debe_TenerDimensionesDe10X10

---
+++ Acorazados.Tests/AcorazadosTests.cs
@@ -0,0 +1,27 @@
+﻿using AwesomeAssertions;
+
+namespace AcorazadosTests;
+
+public class AcorazadosTests
+{
+    [Fact]
+    public void Si_InicializoUnTablero_Debe_TenerDimensionesDe10X10()
+    {
+        var acorazados = new Acorazados(10, 10);
+
+        acorazados.TieneDimensiones(10, 10).Should().BeTrue();
+    }
+}
+
+public class Acorazados
+{
+    public Acorazados(int i, int i1)
+    {
+        
+    }
+
+    public bool TieneDimensiones(int i, int i1)
+    {
+        return true;
+    }
+}
