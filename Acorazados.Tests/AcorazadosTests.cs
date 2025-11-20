6ea0fd7700d380574e3a1b6c8519bedebe069395
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-11-20 14:48:03 -0500

    🟥 Si_Jugador1AgregaUnDestroyerEnLaPosicion32ConOrientacionHorizontal_Debe_ImprimirTableroDestroyerDesde32A34

--- Acorazados.Tests/AcorazadosTests.cs
+++ Acorazados.Tests/AcorazadosTests.cs
@@ -178,4 +178,30 @@ public class AcorazadosTests
 
         acorazados.Jugadores[jugador1].ImprimirTablero().Should().Be(expected);
     }
+
+    [Fact]
+    public void Si_Jugador1AgregaUnDestroyerEnLaPosicion32ConOrientacionHorizontal_Debe_ImprimirTableroDestroyerDesde32A34()
+    {
+        var acorazados = new Acorazados();
+        var jugador1 = "jugador 1";
+        string expected = " |0|1|2|3|4|5|6|7|8|9|\r\n" +
+                          "0|g| | | | | | | | | |\r\n" +
+                          "1| | | | | | | | | | |\r\n" +
+                          "2| | | | | | | | | | |\r\n" +
+                          "3| | |d|d|d| | | | | |\r\n" +
+                          "4| | | | | | | | | | |\r\n" +
+                          "5| | | | | | | | | | |\r\n" +
+                          "6| | | | | | | | | | |\r\n" +
+                          "7| | | | | | | | | | |\r\n" +
+                          "8| | | | | | | | | | |\r\n" +
+                          "9| | | | | | | | | | |\r\n";
+        
+        acorazados.AgregarJugador(jugador1);
+        acorazados.Jugadores[jugador1].AgregarGunShip(0, 0);
+        acorazados.Jugadores[jugador1].AgregarDestroyer(3,2, Orientacion.Horizontal);
+
+        acorazados.Jugadores[jugador1].ImprimirTablero().Should().Be(expected);
+    }
+    
+    
 }
